dfe43a4e38de05346bbf4cddd236cab88cfa9dde
Khazz <thelenaid@gmail.com>
2026-06-09 11:15:29 +0700

    new node

--- searchtree.cpp
+++ searchtree.cpp
@@ -30,6 +30,17 @@ public:
 
     void insert(string element)
     {
+        node *newnode = new node(element, NULL, NULL);
+        
+        newnode->info = element;
+        newnode->leftchild = NULL;
+        newnode->rightchild = NULL;
+
+        node *parent = NULL;
+        node *currentnode = NULL;
+
         
     }
+
+    
 };
